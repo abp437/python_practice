d65df139cda211d4c1cff1fd14870b485b9d438e
abp437 <abp437@gmail.com>
2024-08-09 14:23:04 +0530

    Enable user to exit the program on typing `exit` - Make the behavior like Python Command Prompt - Make the program run infinitely - Use constants - Use one line functions

--- calc_hours.py
+++ calc_hours.py
@@ -1,5 +1,9 @@
 import math
 
+# Constants
+CMD_STR = ">>> "
+DEFAULT_PROMPT = f"Hello, please enter the number of days and I'll calculate the number of hours in it. \nType 'exit' or 'quit' to exit the program. \n{CMD_STR}"
+
 # In python we have to define the function before it's called.
 # There's no concept of hoisting in it, even if it's an interpreted language.
 def validate_number_input(input):
@@ -25,8 +29,27 @@ def calculate_hours(days):
 
   return day_count * 24
 
+# One line functions
+def is_exit_string(s): return s == "exit" or s == "quit"
+
+def is_blank(s): return s == ""
+
+def get_prompt_str(iteration, prev_input):
+  if iteration == 0: return DEFAULT_PROMPT
+  return CMD_STR if is_blank(prev_input) else DEFAULT_PROMPT
+
 def main():
-  user_input = input("Hello, please enter the number of days and I'll calculate the number of hours in it. \n")
-  print(f"The number of hours are {calculate_hours(user_input)}")
+  prev_input = ""
+  i = 0
+
+  while True:
+    user_input = input(get_prompt_str(i, prev_input))
+    prev_input = user_input
+    i += 1
+
+    if is_blank(user_input): continue
+    if is_exit_string(user_input): return
+
+    print(f"The number of hours are {calculate_hours(user_input)}\n")
 
 main()
